b527eab7f359c16dd4a765b5adfac80633b1f3c9
jakubBone <jakub.bone1990@gmail.com>
2025-09-05 12:47:34 +0200

    config: add ProductConfiguration

---
+++ src/main/java/com/jakubbone/checkout/config/ProductConfiguration.java
@@ -0,0 +1,41 @@
+package com.jakubbone.checkout.config;
+
+import com.jakubbone.checkout.domain.ComboOffer;
+import com.jakubbone.checkout.domain.Product;
+import com.jakubbone.checkout.domain.SpecialOffer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@Configuration
+public class ProductConfiguration {
+    @Bean
+    public Map<String, Product> products(){
+        return Map.of(
+                ("A", new Product("A", new BigDecimal(40.0)),
+                ("B", new Product("A", new BigDecimal(10.0)),
+                ("C", new Product("A", new BigDecimal(30.0)),
+                ("D", new Product("A", new BigDecimal(25.0))
+        );
+    }
+
+    @Bean
+    public Map<String, SpecialOffer> specialOffers(){
+        return Map.of(
+                "A", new SpecialOffer("A", 3, new BigDecimal("30")),
+                "B", new SpecialOffer("B", 2, new BigDecimal("7.5")),
+                "C", new SpecialOffer("C", 4, new BigDecimal("20")),
+                "D", new SpecialOffer("D", 2, new BigDecimal("23.5"))
+        );
+    }
+
+    @Bean
+    public Map<String, ComboOffer> comboOffers() {
+        return Map.of(
+                "A/C", new ComboOffer("A", "C", new BigDecimal("-10")),
+                "B/D", new ComboOffer("B", "D", new BigDecimal("-10"))
+        );
+    }
+}
