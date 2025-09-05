60c3c0fb2ae0d29bc20fb19cdc918e563817c854
jakubBone <jakub.bone1990@gmail.com>
2025-09-05 12:56:08 +0200

    feat: create ProductService

---
+++ src/main/java/com/jakubbone/checkout/service/ProductService.java
@@ -0,0 +1,23 @@
+package com.jakubbone.checkout.service;
+
+import com.jakubbone.checkout.domain.ComboOffer;
+import com.jakubbone.checkout.domain.Product;
+import com.jakubbone.checkout.domain.SpecialOffer;
+import lombok.Getter;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+@Getter
+public class ProductService {
+    Map<String, Product> products;
+    Map<String, SpecialOffer> specialOffers;
+    Map<String, ComboOffer> comboOffers;
+
+    public ProductService(Map<String, Product> products, Map<String, SpecialOffer> specialOffers, Map<String, ComboOffer> comboOffers) {
+        this.products = products;
+        this.specialOffers = specialOffers;
+        this.comboOffers = comboOffers;
+    }
+}
