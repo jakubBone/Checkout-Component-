dea68a5bee3b74829a6dd86bfb273d0199a6c471
jakubBone <jakub.bone1990@gmail.com>
2025-09-07 16:52:58 +0200

    feat: implement service's total price calculating method

--- src/main/java/com/jakubbone/checkout/service/CheckoutService.java
+++ src/main/java/com/jakubbone/checkout/service/CheckoutService.java
@@ -1,10 +1,14 @@
 package com.jakubbone.checkout.service;
 
+import com.jakubbone.checkout.domain.BundleOffer;
 import com.jakubbone.checkout.domain.CartItem;
 import com.jakubbone.checkout.domain.Product;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,18 +23,49 @@ public class CheckoutService {
         this.priceCalculator = priceCalculator;
     }
 
-    public void scanItem(String sku){
+    public void scanItem(String sku) {
         Product product = productService.getProduct(sku);
 
-        CartItem existingItem = cart.get(sku);
-        if (existingItem != null) {
-            existingItem.addQuantity(1);
-        } else {
-            cart.put(sku, new CartItem(product, 1));
-        }
+        cart.compute(sku, (key, item) -> {
+            if (item == null) {
+                return new CartItem(product, 1);
+            } else {
+                item.addQuantity(1);
+                return item;
+            }
+        });
     }
 
+    public BigDecimal calculateTotal() {
+        // Calculate the total price with multi-buy offers applied
+        BigDecimal subTotal = BigDecimal.ZERO;
 
+        for (CartItem item : cart.values()) {
+            subTotal = subTotal.add(
+                    priceCalculator.calculatePrice(
+                            item.getProduct(),
+                            item.getQuantity(),
+                            productService.getMultiBuyOffer(item.getProduct().sku())
+                    )
+            );
+        }
 
+        // Apply bundle discounts
+        BigDecimal bundleDiscounts = BigDecimal.ZERO;
+        for (BundleOffer bundleOffer : productService.getBundleOffers()) {
+            CartItem item1 = cart.get(bundleOffer.sku1());
+            CartItem item2 = cart.get(bundleOffer.sku2());
 
+            if (item1 != null && item2 != null) {
+                int numberOfBundles = Math.min(item1.getQuantity(), item2.getQuantity());
+                if (numberOfBundles > 0) {
+                    BigDecimal totalDiscountForOffer = bundleOffer.discount().multiply(BigDecimal.valueOf(numberOfBundles));
+                    bundleDiscounts = bundleDiscounts.add(totalDiscountForOffer);
+                }
+            }
+        }
+
+        return subTotal.subtract(bundleDiscounts).setScale(2, RoundingMode.HALF_UP);
+    }
 }
+
