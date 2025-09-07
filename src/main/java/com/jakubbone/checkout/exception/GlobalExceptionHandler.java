9ec75f81d10403fb6f751057ebf034ebaabf93f6
jakubBone <jakub.bone1990@gmail.com>
2025-09-07 09:20:51 +0200

    feat: add generic exception handler

--- src/main/java/com/jakubbone/checkout/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jakubbone/checkout/exception/GlobalExceptionHandler.java
@@ -1,11 +1,8 @@
 package com.jakubbone.checkout.exception;
 
-import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
@@ -25,4 +22,14 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResp);
     }
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleGenericException(Exception ex){
+        Map<String, Object> errorResp = Map.of(
+            "timestamp", LocalDateTime.now(),
+                "status", HttpStatus.INTERNAL_SERVER_ERROR),
+                "error", "Internal server error",
+                "message", "An unexpected error occurred"
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResp);
+    }
 }
