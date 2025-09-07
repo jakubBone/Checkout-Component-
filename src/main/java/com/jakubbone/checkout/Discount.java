0d3eb3ced86b37214f2aaffea5fa4ac3ad232f7b
jakubBone <jakub.bone1990@gmail.com>
2025-09-07 10:15:25 +0200

    feat: removed Discount interface and its implementations

--- src/main/java/com/jakubbone/checkout/Discount.java
+++
@@ -1,9 +0,0 @@
-package com.jakubbone.checkout;
-
-import com.jakubbone.checkout.domain.Product;
-
-import java.util.Map;
-
-public interface Discount {
-    double apply(Map<Product, Integer> basketPosition);
-}
