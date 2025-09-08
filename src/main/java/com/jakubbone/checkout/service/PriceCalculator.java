73ae7650e466f3afbdeedd335d34e66867c75acb
jakubBone <jakub.bone1990@gmail.com>
2025-09-08 09:05:25 +0200

    fix: change calculation logic for multi buy offer

--- src/main/java/com/jakubbone/checkout/service/PriceCalculator.java
+++ src/main/java/com/jakubbone/checkout/service/PriceCalculator.java
@@ -14,7 +14,15 @@ public class PriceCalculator {
     public BigDecimal calculatePrice(Product product, int quantity, Optional<MultiBuyOffer> offerOpt) {
         if (offerOpt.isPresent() && quantity >= offerOpt.get().requiredQuantity()) {
             MultiBuyOffer offer = offerOpt.get();
-            return offer.specialPrice().multiply(BigDecimal.valueOf(quantity)).setScale(2, RoundingMode.HALF_UP);
+            int offerQuantity = offer.requiredQuantity();
+
+            int numberOfBundles = quantity / offerQuantity;
+            int remainingItems = quantity % offerQuantity;
+
+            BigDecimal bundlePrice = offer.specialPrice().multiply(BigDecimal.valueOf(numberOfBundles));
+            BigDecimal remainingPrice = product.price().multiply(BigDecimal.valueOf(remainingItems));
+
+            return bundlePrice.add(remainingPrice).setScale(2, RoundingMode.HALF_UP);
         }
         return product.price().multiply(BigDecimal.valueOf(quantity)).setScale(2, RoundingMode.HALF_UP);
     }
